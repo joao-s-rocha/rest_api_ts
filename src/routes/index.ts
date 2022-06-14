f307e41391bfa5c8ab28849771a8571224185988
joao-s-rocha <joao.roha25@gmail.com>
2022-06-13 20:22:09 -0400

    teste

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,5 @@ import { alunoRouter } from "./aluno";
 export const useRoutes = (app: Application) => {
   const apiRouter = Router();
   apiRouter.use('/alunos', alunoRouter);
-
   app.use('/', apiRouter);
 }
